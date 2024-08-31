47afa14a3fca0e375b522eacc2c89da22430e380
Ginofvs <gvillavicenciosifuentes@gmail.com>
2024-08-30 21:17:17 -0500

    seguir avanzando en universidad

--- mi_sitio_prueba/blog/models.py
+++ mi_sitio_prueba/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#modelos son la manera de gestionar los datos en la bd 
+
+class Post(models.Model):
+    title = models.CharField(max_length=200) #  titulo de la publicacion
+    content = models.TextField # contenido de la publi
+    author = models.CharField(max_length=100) # campo para el autor de la publi
+    creater_at = models.DateTimeField(auto_now_add=True) # auto_now_add para q sea automaticamente puesta la fecha
+
+    def __str__(self):
+        return self.title
+    #retorna el titulo de la publicacion a string
--- mi_sitio_prueba/blog/views.py
+++ mi_sitio_prueba/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Pagina Principal")
-
+    posts = Post.objects.all() # este posts = Post.objects.all()
+    # obtiene tdas las publicaciones de la bd
+    return render(request,'blog/home.html',{ 'posts' : posts}) # para renderizar la plantilla con las publicaciones ? 
